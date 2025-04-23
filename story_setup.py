3a6564a04a23b6d036138631fe94820e02c162f4
emma <eqbrown@isf.edu.hk>
2025-04-23 08:18:00 +0800

    minor updates to story

--- story_setup.py
+++ story_setup.py
@@ -4,7 +4,7 @@ main_story = Story(
     title='My First Story',
     first_id = 'cks',
     first_option_title = "You are in CKS",
-    first_description= "It's the start of the day! You're hungry and want cookies. Try to collect cookies!",
+    first_description= "It's the start of the day!",
 )
 
 main_story.add_new_child(
@@ -18,14 +18,14 @@ main_story.add_new_child(
     parent_id = 'cks', 
     child_id = 'b2',
     child_option_title='You walk up the stairs to B2',
-    child_description="You a few students noodling on the piano. You smell cookies to the left.",
+    child_description="You a few students noodling on the piano.",
 )
 
 main_story.add_new_child(
     parent_id = 'b2', 
     child_id = 'makerspace',
     child_option_title='You turn left and enter the makerspace',
-    child_description="You see a sea of 3D printers making noises and making art. And see a cookie!",
+    child_description="You see a sea of 3D printers making noises and making art.",
 )
 
 
